8b76781a12f7595f6ddae9a7b93451c17e3b6a6a
Rafal Soczek <rafalssoczek@gmail.com>
2022-04-02 15:33:55 +0200

    implemented generation of receipEntries's list

--- src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
+++ src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -1,14 +1,24 @@
 package com.virtuslab.internship.receipt;
 
 import com.virtuslab.internship.basket.Basket;
+import com.virtuslab.internship.product.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
 
 public class ReceiptGenerator {
 
     public Receipt generate(Basket basket) {
         List<ReceiptEntry> receiptEntries = new ArrayList<>();
-        return new Receipt(receiptEntries);
+        Map<Product,Long> productQuantity = basket.getProducts().stream()
+                .collect(groupingBy(e->e, Collectors.counting()));
+        for (var entry : productQuantity.entrySet()) {
+            receiptEntries.add(new ReceiptEntry(entry.getKey(),(entry.getValue().intValue())));
+        }
+        return new Receipt (receiptEntries);
     }
 }
